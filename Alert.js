a0bbf684de92c49455b813adabd27969f5379022
Seb le Roux <sebastienlucleroux@gmail.com>
2025-11-11 10:37:56 +0200

    Update feature X and fix issue Y

--- Alert.js
+++ Alert.js
@@ -8,19 +8,19 @@ var temperatureAlertActive = [false, false, false, false, false, false, false, f
 var humidityAlertActive = [false, false, false, false, false, false, false, false, false, false, false];
 var pressureAlertActive = [false, false, false, false, false, false, false, false, false, false, false];
 
-function fetchData(){
+function fetchData() {
   // testDataArray = logged readings
   const testDataArray = ["2022/07/26", "10:00:00", "19", "0.37", "48"];
   return testDataArray;
 }
 
 function writeData() {
-  const testDataArray = fetchData()
+  const testDataArray = fetchData();
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[2];
   const lastRow = sheet.getLastRow();
-  newRowNumber = lastRow+1;
-  for(let i=0; i<5; i++) {
-    const cell = sheet.getRange(newRowNumber, (i+1));
+  newRowNumber = lastRow + 1;
+  for (let i = 0; i < 5; i++) {
+    const cell = sheet.getRange(newRowNumber, (i + 1));
     cell.setValue(testDataArray[i]);
   }
   checkData();
@@ -28,46 +28,46 @@ function writeData() {
 
 function checkData() {
 
-var temperatureAlertActive = [false, false, false, false, false, false, false, false, false, false, false];
-var humidityAlertActive = [false, false, false, false, false, false, false, false, false, false, false];
-var pressureAlertActive = [false, false, false, false, false, false, false, false, false, false, false];
+  var temperatureAlertActive = [false, false, false, false, false, false, false, false, false, false, false];
+  var humidityAlertActive = [false, false, false, false, false, false, false, false, false, false, false];
+  var pressureAlertActive = [false, false, false, false, false, false, false, false, false, false, false];
 
-  let alertInfo = getAlertInfo()
+  let alertInfo = fetchAlertInfo();
   //i < Total number of locations
   for (let i = 0; i < 11; i++) {
     const testDataArray = fetchData();
     if (JSON.parse(testDataArray[2]) >= alertInfo[i][1] || JSON.parse(testDataArray[2]) <= alertInfo[i][2]) {
-      if(!temperatureAlertActive[i]) {
+      if (!temperatureAlertActive[i]) {
         temperatureAlertActive[i] = true;
         temperatureAlert(i);
       }
-    }else{
-      if(temperatureAlertActive[i]) {
-        temperatureAlertActive[i] = false;        
+    } else {
+      if (temperatureAlertActive[i]) {
+        temperatureAlertActive[i] = false;
         clearTemperatureAlert(i);
       }
     }
 
     if (JSON.parse(testDataArray[3]) >= alertInfo[i][3] || JSON.parse(testDataArray[3]) <= alertInfo[i][4]) {
-      if(!humidityAlertActive[i]) {
+      if (!humidityAlertActive[i]) {
         humidityAlertActive[i] = true;
         humidityAlert(i);
       }
-    }else{
-      if(humidityAlertActive[i]) {
+    } else {
+      if (humidityAlertActive[i]) {
         humidityAlertActive[i] = false;
         clearHumidityAlert(i);
       }
     }
 
-    if(testDataArray[4]) {
+    if (testDataArray[4]) {
       if (JSON.parse(testDataArray[4]) >= alertInfo[i][5] || JSON.parse(testDataArray[4]) <= alertInfo[i][6]) {
-        if(!pressureAlertActive[i]) {
+        if (!pressureAlertActive[i]) {
           pressureAlertActive[i] = true;
           pressureAlert(i);
         }
-      }else{
-        if(pressureAlertActive[i]) {
+      } else {
+        if (pressureAlertActive[i]) {
           pressureAlertActive[i] = false;
           clearPressureAlert(i);
         }
@@ -76,9 +76,9 @@ var pressureAlertActive = [false, false, false, false, false, false, false, fals
   }
 }
 
-function getUnformattedAlertInfo(){
+function getUnformattedAlertInfo() {
 
-  
+
   //13 = index of alert info sheet
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[13];
   //11 = Total number of locations
@@ -89,9 +89,9 @@ function getUnformattedAlertInfo(){
   return alertInfo;
 }
 
-function getAlertInfo(){
+function fetchAlertInfo() {
   const alertInfo = getUnformattedAlertInfo();
-  for (let i = 0; i < 11; i++){
+  for (let i = 0; i < 11; i++) {
     const emailArray = alertInfo[i][8].split(";");
     alertInfo[i][8] = emailArray;
   }
@@ -99,7 +99,7 @@ function getAlertInfo(){
 }
 
 function temperatureAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   for (let j = 0; j < alertInfo[i][8].length; j++) {
     var recipient = alertInfo[i][8][j];
@@ -113,7 +113,7 @@ Reading: ${testDataArray[2]}°C`;
 }
 
 function clearTemperatureAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   for (let j = 0; j < alertInfo[i][8].length; j++) {
     var recipient = alertInfo[i][8][j];
@@ -128,7 +128,7 @@ Reading: ${testDataArray[2]}°C`;
 
 
 function humidityAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   for (let j = 0; j < alertInfo[i][8].length; j++) {
     var recipient = alertInfo[i][8][j];
@@ -136,27 +136,27 @@ function humidityAlert(i) {
     var body = `Date: ${testDataArray[0]}
 Time: ${testDataArray[1]}
 Location: ${alertInfo[i][0]}
-Reading: ${(testDataArray[3])*100}%`;
+Reading: ${(testDataArray[3]) * 100}%`;
     MailApp.sendEmail(recipient, subject, body);
   }
 }
 
 function clearHumidityAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   for (let j = 0; j < alertInfo[i][8].length; j++) {
     var recipient = alertInfo[i][8][j];
     var subject = `Abnormal Humidity Reading No Longer Detected`;
-    var body =`Date: ${testDataArray[0]}
+    var body = `Date: ${testDataArray[0]}
 Time: ${testDataArray[1]}
 Location: ${alertInfo[i][0]}
-Reading: ${(testDataArray[3])*100}%`;
+Reading: ${(testDataArray[3]) * 100}%`;
     MailApp.sendEmail(recipient, subject, body);
   }
 }
 
 function pressureAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   for (let j = 0; j < alertInfo[i][8].length; j++) {
     var recipient = alertInfo[i][8][j];
@@ -170,7 +170,7 @@ Reading: ${testDataArray[4]}kPa`;
 }
 
 function clearPressureAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   for (let j = 0; j < alertInfo[i][8].length; j++) {
     var recipient = alertInfo[i][8][j];
@@ -185,7 +185,7 @@ Reading: ${testDataArray[4]}kPa`;
 
 /*
 function temperatureAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   var recipient = alertInfo[i][8];
   var subject = `Abnormal Temperature Reading Detected`;
@@ -197,7 +197,7 @@ function temperatureAlert(i) {
 }
 
 function clearTemperatureAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   var recipient = alertInfo[i][8];
   var subject = `Abnormal Temperature Reading No Longer Detected`;
@@ -209,7 +209,7 @@ function clearTemperatureAlert(i) {
 }
 
 function humidityAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   var recipient = alertInfo[i][8];
   var subject = `Abnormal Humidity Reading Detected`;
@@ -221,7 +221,7 @@ function humidityAlert(i) {
 }
 
 function clearHumidityAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   var recipient = alertInfo[i][8];
   var subject = `Abnormal Humidity Reading No Longer Detected`;
@@ -233,7 +233,7 @@ function clearHumidityAlert(i) {
 }
 
 function pressureAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   var recipient = alertInfo[i][8];
   var subject = `Abnormal Pressure Reading Detected`;
@@ -245,7 +245,7 @@ function pressureAlert(i) {
 }
 
 function clearPressureAlert(i) {
-  const alertInfo = getAlertInfo();
+  const alertInfo = fetchAlertInfo();
   const testDataArray = fetchData();
   var recipient = alertInfo[i][8];
   var subject = `Abnormal Pressure Reading No Longer Detected`;
